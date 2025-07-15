70f98a9de3fb280718ee78a726e9a13add7d3d37
Khoi-de-boi <khoi-de-boi@KhoideboisAir.lan>
2025-07-14 23:08:32 -0700

    added metadata

--- src/main/java/Tangwing/Cloud/CloudApplication.java
+++ src/main/java/Tangwing/Cloud/CloudApplication.java
@@ -2,12 +2,24 @@ package Tangwing.Cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
+	@GetMapping("/bitch")
+	public String hello() {
+      return String.format("bitch\n");
+    }
 }
---
+++ src/main/java/Tangwing/Cloud/model.java
@@ -0,0 +1,23 @@
+package Tangwing.Cloud.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileMetaData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String filename;
+    private String Path;
+    private Long size;
+    private LocalDateTime uploadedAt;
+}
+
