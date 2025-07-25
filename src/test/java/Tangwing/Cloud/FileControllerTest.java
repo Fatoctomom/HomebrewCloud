675440b7cbdee0f705a732ca46f8f6de859bcaf1
Thangwing <bobball304@gmail.com>
2025-07-25 15:44:24 -0700

    rest controller tested

--- src/test/java/Tangwing/Cloud/FileControllerTest.java
+++ src/test/java/Tangwing/Cloud/FileControllerTest.java
@@ -1,6 +1,7 @@
 package Tangwing.Cloud;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,36 +10,44 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
 public class FileControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @Order(1)
     void testFileUploadList() throws Exception {
         // upload
-        MockMultipartFile testFile = new MockMultipartFile(
-            "testFile",                        //name
+        MockMultipartFile file = new MockMultipartFile(
+            "file",                        //name
             "testfile.txt",    //original file name
             "text/plain",           //contentType
             "hello world!".getBytes()            //content
             );
 
-            mockMvc.perform(multipart("/api/files/upload").file(testFile))
+            mockMvc.perform(multipart("/api/files/upload").file(file).with(csrf()))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.filename").value("testfile.txt"));
+            .andExpect(jsonPath("$.fileName").value("testfile.txt"));
 
         // list
             mockMvc.perform(get("/api/files/list"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].filename").value("testfile.txt"));
+            .andExpect(jsonPath("$[0].fileName").value("testfile.txt"));
     }
 
 
     @Test
+    @Order(2)
     void testDownloadFile() throws Exception {
         // Download
         mockMvc.perform(get("/api/files/download/testfile.txt"))
@@ -48,9 +57,10 @@ public class FileControllerTest {
     }
 
     @Test
+    @Order(3)
     void testDeleteFile() throws Exception {
         //Deletion
-        mockMvc.perform(delete("api/files/testfile.txt"))
+        mockMvc.perform(delete("/api/files/testfile.txt"))
             .andExpect(status().isNoContent());
     }
 }
